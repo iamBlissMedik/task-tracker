88f1700d5723ae2199da42f7c516b0d0157eebed
Ellams Bliss <ellamsbliss@gmail.com>
2023-07-26 19:04:44 +1000

    button function prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,17 @@
-const Button = () => {
-  return <div className="btn">Add</div>;
+import PropTypes from "prop-types";
+const Button = ({ color, text, onClick }) => {
+  return (
+    <div onClick={onClick} style={{ backgroundColor: color }} className="btn">
+      {text}
+    </div>
+  );
+};
+Button.defaultProps = {
+  color: "steelblue",
+};
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func.isRequired
 };
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 const Header = ({ title }) => {
+      const onClick = (e) => {
+        console.log(e);
+      };
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button />
+      <Button onClick={onClick} color="green" text="Hello" />
     </header>
   );
 };
