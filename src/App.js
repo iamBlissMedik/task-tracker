cdb883dea260d8fd1bad6e21cb3cfb78672e4184
Ellams Bliss <ellamsbliss@gmail.com>
2023-07-27 22:02:30 +1000

    toggle reminder

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ const App = () => {
   const [tasks, setTasks] = useState([]);
   useEffect(() => {
     const getTasks = async () => {
-      const tasksFromServer = await fetchTask();
+      const tasksFromServer = await fetchTasks();
       setTasks(tasksFromServer);
     };
     getTasks();
   }, []);
   // fetch task
-  const fetchTask = async () => {
+  const fetchTasks = async () => {
     const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
+  // fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await res.json();
     return data;
   };
@@ -41,10 +47,20 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
   // toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+    const data = await res.json();
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
