278ab81ecb49d588709f52555c1d0c5b85a1fb4b
Ellams Bliss <ellamsbliss@gmail.com>
2023-07-27 23:00:12 +1000

    react router clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation();
   const onClick = (e) => {
     onAdd();
   };
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        onClick={onClick}
-        color={showAddTask ? "red" : "green"}
-        text={showAddTask ? "Close" : "Add"}
-      />
+      {location.pathname === "/" && (
+        <Button
+          onClick={onClick}
+          color={showAddTask ? "red" : "green"}
+          text={showAddTask ? "Close" : "Add"}
+        />
+      )}
     </header>
   );
 };
